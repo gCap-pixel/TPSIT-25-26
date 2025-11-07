b19e6d88d24d4a8c6e1e2a67867113da7e22234d
gCap-pixel <gcappellin9@gmail.com>
2025-11-07 12:17:57 +0100

    gioco funzionante, creo tot macchine e poi avviando il gioco le macchine fanno dei metri rando. Il primo che arriva a 50 vince. Devo sistemare la gestione perchè devo fare in modo che quando la prima macchina supera i 50 metri le altre macchine si devono fermare

--- Gara auto/src/Auto.java
+++ Gara auto/src/Auto.java
@@ -1,7 +1,41 @@
-public class Auto  implements Runnable{
+import java.util.Random;
+
+
+public class Auto  extends Thread {
+    private String colore;
+    private int metriPercorsi = 0;
+    private Random random = new Random();
+    private boolean inGara = true;
+
+    public Auto(String colore) {
+        this.colore = colore;
+    }
+
+    public String getColore() {
+        return colore;
+    }
+
+    public int getMetriPercorsi() {
+        return metriPercorsi;
+    }
 
     @Override
-    public void run() {
-//ogni secondo fa tot metri
+    public void run(){
+        while (inGara){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+               System.out.println("errore");
+            }
+            int passo = random.nextInt(10) + 1; // tra 1 e 10 metri
+            metriPercorsi += passo;
+            System.out.println("macchina "+colore+" ha fatto "+passo+" metri!");
+            System.out.println ("macchina "+colore+" ha percorso in totale "+getMetriPercorsi());
+
+            if (metriPercorsi>=50){
+                inGara=false;
+                System.out.println("macchina di colore "+colore+" vince la gara!!");
+            }
+        }
     }
 }
--- Gara auto/src/Gara.java
+++ Gara auto/src/Gara.java
@@ -1,7 +1,10 @@
 public class Gara {
     public static void main(String[] args){
-        Auto a = new Auto();
-        Thread ta = new Thread(a);
-        ta.start();
+        Auto a = new Auto("viola");
+        Auto b = new Auto("rosso");
+        Thread n = new Thread(a);
+        Thread m = new Thread(b);
+        n.start();
+        m.start();
         }
 }
